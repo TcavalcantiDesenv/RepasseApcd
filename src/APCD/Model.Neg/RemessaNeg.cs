fd58d19ff23b62b4de2b2699fe54426c8be8c464
Hilton Sousa Lima <hiltonslima@hotmail.com>
2020-04-10 17:54:09 -0300

    Primeira Carga

---
+++ src/APCD/Model.Neg/RemessaNeg.cs
@@ -0,0 +1,55 @@
+﻿using Model.Dao;
+using Model.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Neg
+{
+   public class RemessaNeg 
+    {
+        private RemessaDao objRemessaDao;
+        public RemessaNeg()
+        {
+            objRemessaDao = new RemessaDao();
+        }
+
+        public List<RemessaWeb> findAll()
+        {
+            return objRemessaDao.findAll();
+        }
+        public List<RemessaWeb> BuscaPorRegional(string regional, bool pago, string ativo, string mesano)
+        {
+            return objRemessaDao.BuscaPorRegional(regional, pago, ativo, mesano);
+        }
+        public List<RemessaWeb> BuscaPorCodRegional(string regional, string mesano)
+        {
+            return objRemessaDao.BuscaPorCodRegional(regional, mesano);
+        }
+        public List<RemessaWeb> BuscaPagos(string ativo)
+        {
+            return objRemessaDao.BuscaPagos(ativo);
+        }
+        public List<RemessaWeb> BuscaPorId(string Id)
+        {
+            return objRemessaDao.BuscaPorId(Id);
+        }
+        public void update(RemessaWeb objRemessa)
+        {
+            objRemessaDao.update(objRemessa);
+            return;
+        }
+        public void baixaLote(string Pago, string Ativo, string CodRegional, string MesAno, string ListaLCM)
+        {
+            objRemessaDao.baixaLote(Pago, Ativo, CodRegional, MesAno, ListaLCM);
+            return;
+        }
+        public void create(RemessaWeb objMacon)
+        {
+            objRemessaDao.create(objMacon);
+            return;
+        }
+    }
+}
